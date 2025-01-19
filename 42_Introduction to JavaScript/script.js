1ee0e580c57bcb60d4b7e451226a31ba0931b103
Harsh Shukla <talktoharshshukla@gmail.com>
2025-01-19 15:28:11 +0530

    Day 14 - Netflix UI Clone - 100%
    Remaining Steps : Optimizing CSS code

---
+++ 42_Introduction to JavaScript/script.js
@@ -0,0 +1,18 @@
+
+// This is a printing statement 
+console.log("JS is running")
+
+
+// Using prompt to take input from user 
+var a = prompt("Enter :")
+console.log("You entered : "+ a)
+
+
+// Using if-else
+var closingTab = confirm("Are you sure?")
+if (closingTab) {
+    console.log("Please don't leave us")
+}
+else{
+    console.log("let's build something")
+}
