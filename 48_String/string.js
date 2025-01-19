c4c36df9d22da181686d1a761ab2c21c4b51199c
Harsh Shukla <talktoharshshukla@gmail.com>
2025-01-20 00:08:15 +0530

    JS (Functions - String) + Exercise

---
+++ 48_String/string.js
@@ -0,0 +1,11 @@
+// Comtinue at 6:47
+
+let str = "Harsh"
+
+console.log(str.length); // Output : 5
+console.log(str.replaceAll("h","s")); // Output : sarsh  ----->  replaceAll "h" with "s"
+
+// Template literals
+
+console.log("My name is : " + str); // Usual Way
+console.log(`My name is : ${str}`); // Template literals
