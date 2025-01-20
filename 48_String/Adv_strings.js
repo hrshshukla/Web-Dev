a7c718a2f6bb06bff1ddfded5f4ffbcc9b71ea87
Harsh Shukla <talktoharshshukla@gmail.com>
2025-01-20 20:39:02 +0530

    Day 15 - String Basics

---
+++ 48_String/Adv_strings.js
@@ -0,0 +1,38 @@
+// Lowercase & UpperCase
+
+    let name = "Harsh"
+    console.log( "Usign -> toUpperCase() : " + name.toUpperCase()); // Output : HARSH
+    console.log( "Usign -> toLowerCase() : " + name.toLowerCase()); // Output : harsh
+    console.log( "Length : " + name.length); // Output : 5
+    
+// slice()
+
+    let BroName = "Adarsh"
+    console.log(BroName.slice(2)); // Output : arsh
+    console.log(BroName.slice(2,4)); // Output : slice( [start], [end] )
+
+// Replace & concatinate  ---> replace() & concat()
+
+    let MomName = "Pratibha"
+    MomName = MomName.replace("Pratibha", "Pratima"); // Pratibha --> [replaced] --> Pratima
+    console.log(MomName); // MomName = "Pratima"
+
+    console.log(MomName.concat(BroName, "successfully concatinated!!"));
+    console.log("MomName");
+
+// Remove white space ---> trim()
+
+    let fatherName = "  Ayodhya   "
+    console.log(fatherName);        // Output : [  Ayodhya   ]
+    console.log(fatherName.trim()); // Output : Ayodhya
+
+// Index 
+    console.log(MomName[0]);
+    console.log(MomName.indexOf("ti")); // 3
+
+// 
+    console.log(MomName.startsWith("Pra")); // true ----> becasue "Pratima" is starting with "Pra"
+    console.log(MomName.endsWith("bha")); // false ----> becasue "Pratima" do not ends with "bha"
+    
+    
+    
--- 48_String/string.js
+++ 48_String/string.js
@@ -5,7 +5,19 @@ let str = "Harsh"
 console.log(str.length); // Output : 5
 console.log(str.replaceAll("h","s")); // Output : sarsh  ----->  replaceAll "h" with "s"
 
-// Template literals
+
+// _______________________________Template literals________________________________________________________
 
 console.log("My name is : " + str); // Usual Way
+console.log(`The password is "harsh@123" `);// We can insert double-quote "" using Template literals 
+
+// When we insert variables directly in template literals, this is called string interpolation
 console.log(`My name is : ${str}`); // Template literals
+
+
+
+//__________________________ Escape Sequence Character_________________________________________________________________
+console.log("harsh \n shukla"); // Output : "\n" for new line 
+console.log("harsh \" shukla"); // Output : harsh " shukla
+ 
+
